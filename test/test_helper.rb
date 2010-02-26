6b2b5712544c00c8587a53858b3d42697159a4f0
alexdunae <alex@dunae.ca>
2010-02-26 15:15:54 -0800

    Fix up test loader

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__), '../'))
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__), '../lib/'))
 require 'rubygems'
 require 'test/unit'
-require 'css_parser'
 require 'net/http'
 require 'open-uri'
 require 'WEBrick'
+require File.dirname(__FILE__) + '/../lib/css_parser'
