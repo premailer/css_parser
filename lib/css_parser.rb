3e0af4f067fe9ca2271c3d564f88c1089876c40f
Alex Dunae <alex@dunae.ca>
2010-11-03 16:40:10 -0700

    Pre-release doc updates

--- lib/css_parser.rb
+++ lib/css_parser.rb
@@ -7,7 +7,7 @@ require 'stringio'
 require 'iconv'
 
 module CssParser
-  VERSION = '1.1.2'
+  VERSION = '1.1.3'
 
   # Merge multiple CSS RuleSets by cascading according to the CSS 2.1 cascading rules 
   # (http://www.w3.org/TR/REC-CSS2/cascade.html#cascading-order).
