8f7bf42d7b681b769e8e5572175dbe080819b8aa
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2016-06-28 00:55:47 +0300

    redefine

--- lib/css_parser/parser.rb
+++ lib/css_parser/parser.rb
@@ -189,8 +189,8 @@ module CssParser
     #
     # +media_types+ can be a symbol or an array of symbols.
     # See RuleSet#each_selector for +options+.
-    def each_selector(media_types = :all, options = {}) # :yields: selectors, declarations, specificity, media_types
-      each_rule_set(media_types) do |rule_set, media_types|
+    def each_selector(all_media_types = :all, options = {}) # :yields: selectors, declarations, specificity, media_types
+      each_rule_set(all_media_types) do |rule_set, media_types|
         rule_set.each_selector(options) do |selectors, declarations, specificity|
           yield selectors, declarations, specificity, media_types
         end
