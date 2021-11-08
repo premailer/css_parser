65f18fda4c98bdad14e8a0ca6dbc9eabb5e79b3a
Simon <simon.toivo@telhaug.no>
2021-11-08 01:21:17 +0100

    Support string in propperties

--- lib/css_parser/parser/parser.rb
+++ lib/css_parser/parser/parser.rb
@@ -14,10 +14,7 @@ module CssParser
           }
           scanner.skip(/\s*/)
           rule[:selector] = parse_selector(scanner).strip
-
-          properties = scanner.scan_until(/}/)
-          pp properties
-          rule[:properties] += properties[0..-2].strip
+          rule[:properties] = parse_proppertied(scanner).strip
 
           rules << rule
           puts "pushing rule #{rule.inspect}"
@@ -36,12 +33,8 @@ module CssParser
           selector = scanner.scan_until(/#{quote}/)
           quoted_string += selector
           backslashes = /\\*\z/.match(selector[0..-2])[0].length
-          puts "backslashes #{backslashes} even? #{backslashes.even?}"
           string_ended = backslashes.even?
         end
-        puts "quoted_string"
-        puts quoted_string
-        puts quoted_string.gsub(/\\{2}/, "5")
 
         quoted_string.gsub(/\\{2}/, "\\")
       end
@@ -51,20 +44,13 @@ module CssParser
 
         got_to_properties = false
         while !got_to_properties
-          puts "=" * 60
-
           selector = scanner.scan_until(/'|"|{|\\{2}/)
           case scanner[0]
           when "\\\\" # maybe this can be replaced with a gsub at the end .tr("\\\\", "\\")
-            puts scanner[0].length
-            puts selector
-            puts selector[0..(-1-scanner[0].length)]
             new_selector += selector[0..(- 1 - scanner[0].length)]
             # if you have two backslashes the selector should have one
             new_selector += "\\"
 
-            puts "the selector is now "
-            puts new_selector
           when "\"", "'"
             backslashes = /\\*\z/.match(selector[0..-2])[0].length
             case backslashes
@@ -83,33 +69,50 @@ module CssParser
             # should only be 0 or 1 backslash before { since we scan until will we find an escaped backslash or curly
             backslashes = /\\*\z/
               .match(selector[0..-2])
-              .tap { |a| puts "found #{a.inspect} #{a[0].inspect} length #{a[0].length}" }
               .yield_self { |match| match[0].length }
 
             case backslashes
             when 0
               new_selector += selector[0..-2]
             when 1
-              puts "curly was escaped"
-              puts selector[0..-3]
               new_selector += selector[0..-3] # "remove last \ and {  and append { to selector"
               new_selector += "{"
             else
               raise "should only be 0 or 1 backslash at this location"
             end
 
-            puts "the selector is now "
-            puts new_selector
-
             # If these is an odd number of backslashes it is expected
             got_to_properties = backslashes.even?
           else
-            puts "got something unexpected #{scanner.values_at(0).inspect}"
             raise "got something unexpected #{scanner.values_at(0).inspect}"
           end
         end
 
         new_selector
+      end
+
+      def self.parse_proppertied(scanner)
+        properties = ""
+        end_of_propertied = false
+
+        until end_of_propertied
+          selector = scanner.scan_until(/'|"|}/)
+          case scanner[0]
+          when "\"", "'"
+            backslashes = /\\*\z/.match(selector[0..-2])[0].length
+            raise "Dont think you can have any escaped quates here" unless backslashes == 0
+
+            properties += selector
+            properties += parse_string(scanner, scanner[0])
+          when "}"
+            properties += selector[0..-2]
+            end_of_propertied = true
+          else
+            raise "got something unexpected #{scanner.values_at(0).inspect}"
+          end
+        end
+
+        properties.strip
       end
     end
   end
