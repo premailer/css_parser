569097016810d683ea96d53342baa5c37b7721b1
Simon <simon.toivo@telhaug.no>
2024-05-23 15:36:10 +0200

    Fix spec to actually test exception message
    second argument to assert_raises is custom error message

--- test/rule_set/test_declarations.rb
+++ test/rule_set/test_declarations.rb
@@ -2,6 +2,7 @@
 
 require_relative '../test_helper'
 require 'minitest/spec'
+require 'ostruct'
 
 class RuleSetDeclarationsTest < Minitest::Test
   describe '.new' do
@@ -72,8 +73,11 @@ class RuleSetDeclarationsTest < Minitest::Test
     it 'raises an exception including the property when the value is empty' do
       declarations = CssParser::RuleSet::Declarations.new
 
-      assert_raises ArgumentError, 'foo value is empty' do
+      assert_raises ArgumentError do
         declarations['foo'] = '!important'
+      rescue ArgumentError => e
+        assert_equal e.message, 'foo value is empty'
+        raise e
       end
     end
   end
